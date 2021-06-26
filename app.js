ec3f37a113f181d3f7343266521d6eacb1db944f
fungerouscode <duchanhstyle@gmail.com>
2021-06-26 23:11:30 +0700

    setup view engine using Pug

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT | 3001
 
-// Running Application
+// Setup
+app.set("view engine", "pug");
+app.set("views","views");
 
+// Route
+app.get("/",(req, res, next) => {
+  res.status(200).render("home");
+})
+
+// Running Application
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
