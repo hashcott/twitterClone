64a47f81fdd4a4751aff62aea88bc9607828ad2c
fungerouscode <duchanhstyle@gmail.com>
2021-06-27 18:37:09 +0700

    add body parser

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 const PORT = process.env.PORT | 3001;
 const middleware = require("./middleware");
 const path = require('path');
+const bodyParser = require("body-parser");
 // Setup
 app.set("view engine", "pug");
 app.set("views","views");
 app.use(express.static(path.join(__dirname, "public")))
+app.use(bodyParser.urlencoded({ extended : false }))
 // Route
 const loginRoute = require('./routes/loginRoutes');
 app.use("/login", loginRoute);
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   res.status(200).render("register");
 });
-
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+})
 module.exports = router;
