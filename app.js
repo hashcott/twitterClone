78d8ec6e8890c04051047df149d81520c370159c
fungerouscode <duchanhstyle@gmail.com>
2021-06-27 18:22:50 +0700

    add register

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(express.static(path.join(__dirname, "public")))
 // Route
 const loginRoute = require('./routes/loginRoutes');
 app.use("/login", loginRoute);
+const registerRoute = require('./routes/registerRoutes');
+app.use("/register", registerRoute);
 
 app.get("/", middleware.requireLogin ,(req, res, next) => {
   var payload = {
