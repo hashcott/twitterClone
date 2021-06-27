131e9ca5dfde70b739fd69405446fa547ca6bd8e
fungerouscode <duchanhstyle@gmail.com>
2021-06-28 00:10:08 +0700

    add user schema

--- app.js
+++ app.js
@@ -3,27 +3,37 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT | 3001;
 const middleware = require("./middleware");
-const path = require('path');
+const path = require("path");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 // Setup
 app.set("view engine", "pug");
-app.set("views","views");
-app.use(express.static(path.join(__dirname, "public")))
-app.use(bodyParser.urlencoded({ extended : false }))
+app.set("views", "views");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
 // Route
-const loginRoute = require('./routes/loginRoutes');
+const loginRoute = require("./routes/loginRoutes");
 app.use("/login", loginRoute);
-const registerRoute = require('./routes/registerRoutes');
+const registerRoute = require("./routes/registerRoutes");
 app.use("/register", registerRoute);
 
-app.get("/", middleware.requireLogin ,(req, res, next) => {
+app.get("/", middleware.requireLogin, (req, res, next) => {
   var payload = {
-    title : "Home"
-  }
+    title: "Home",
+  };
   res.status(200).render("home", payload);
-})
+});
 
 // Running Application
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://admin:hanhphuc@cluster0.mni3m.gcp.mongodb.net/twitterClone?retryWrites=true&w=majority",
+      { useUnifiedTopology: true, useNewUrlParser: true }
+    );
+    console.log("Connected to mongodb");
+  } catch (error) {
+    console.log(error.message);
+  }
   console.log(`Server running on port ${PORT}`);
-})
+});
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -6,5 +6,19 @@ router.get("/", (req, res, next) => {
 });
 router.post('/', (req, res, next) => {
   console.log(req.body);
+  let firstName = req.body.firstName.trim();
+  let lastName = req.body.lastName.trim();
+  let username = req.body.username.trim();
+  let password = req.body.password.trim();
+  let passwordConf = req.body.passwordConf.trim();
+
+  let payload = req.body;
+
+  if(firstName && lastName && username && password && passwordConf) {
+    
+  } else {
+    payload.errorMessage = "Make sure each field have a valid value.";
+    res.status(200).render("register", payload);
+  }
 })
 module.exports = router;
