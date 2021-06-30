01adf7b1d45d130bd0db725313daf0958935c11c
fungerouscode <duchanhstyle@gmail.com>
2021-07-01 00:35:16 +0700

    adding click handler to the button submit

--- public/js/common.js
+++ public/js/common.js
@@ -1,6 +1,8 @@
-document.getElementById("postTextarea").addEventListener("keyup", e => {
+let button = document.getElementById("submitPostButton");
+let textarea = document.getElementById("postTextarea");
+
+textarea.addEventListener("keyup", e => {
   let text = e.target.value.trim();
-  let button = document.getElementById("submitPostButton");
   if(text === "") {
     button.disabled = true;
     return;
@@ -9,3 +11,14 @@ document.getElementById("postTextarea").addEventListener("keyup", e => {
   button.disabled = false;
 })
 
+button.addEventListener("click", async () => {
+  let content = textarea.value.trim();
+  let data = {
+    content
+  }
+  await fetch("/api/post", {
+    method : "POST",
+    body : data
+  })
+
+})
